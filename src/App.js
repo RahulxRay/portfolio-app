fbaa14b0bf1760887d6a0949b3fb96fe567ed528
RahulxRay <rr.rayrahul@gmail.com>
2024-05-18 21:54:56 +0100

    chore: Update ag-grid-react and ag-grid-community npm dependencies to version 31.3.2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
-import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import { useState } from 'react';
+"use strict";
 
-function App() {
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ]);
+import React, { useCallback, useMemo, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import "./styles.css";
+import { ClientSideRowModelModule } from "ag-grid-community";
+import { ModuleRegistry } from "ag-grid-community";
 
-  // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
-    {
-      field: "make",
-      editable: true,
-      cellEditor: 'agSelectCellEditor',
-      cellEditorParams: {
-        values: ['Tesla', 'Ford', 'Toyota'],
-      },
-    },
-    { field: "model", filter: true, floatingFilter: true },
-    { field: "price", valueFormatter: p => '£' + p.value.toLocaleString() },
-    { field: "electric", editable: true }
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const App = () => {
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100vh" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState(null);
+  const [columnDefs] = useState([
+    { field: "athlete", minWidth: 170 },
+    { field: "age" },
+    { field: "country" },
+    { field: "year" },
+    { field: "date" },
+    { field: "sport" },
+    { field: "gold" },
+    { field: "silver" },
+    { field: "bronze" },
+    { field: "total" },
   ]);
+  const defaultColDef = useMemo(() => ({
+    editable: true,
+    filter: true,
+  }), []);
+
+  const onGridReady = useCallback((params) => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data));
+  }, []);
+
   return (
-    <div
-      className="ag-theme-quartz" // applying the grid theme
-      style={{ height: 500 }} // the grid will fill the size of the parent container
-    >
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-      />
+    <div style={containerStyle}>
+      <div style={gridStyle} className="ag-theme-quartz">
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          onGridReady={onGridReady}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
